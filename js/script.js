6a9e548b2c884ec000aa3fa2b4fe6d3a88bc83b8
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2018-11-11 21:56:14 +0100

    Add "add-link" modal and fix JS error when opening page not containing referred element

--- js/script.js
+++ js/script.js
@@ -13,21 +13,79 @@ document.querySelector('.navigation__menu-button').addEventListener('click', fun
 
 /* toggle menu end */
 
-document.querySelector('form#personal-data').addEventListener('submit', function(event) {
-    var isFormValid = true;
+var personal_data_form = document.querySelector('form#personal-data');
 
-    /* sprawdzenie adresu email i ewentualne pokolorwanie inputa */
-    var emailAddressInput = event.target.querySelector('input[id="input-email"]');
+// walidacja formularza personal data - email
+if (typeof personal_data_form !== "undefined" && personal_data_form !== null) {
+    personal_data_form.addEventListener('submit', function(event) {
+        var isFormValid = true;
 
-    if(emailAddressInput.value.indexOf('@') < 0) {
-        isFormValidate = false;
-        emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
-        emailAddressInput.classList.add('error');
-    } else {
-        emailAddressInput.parentElement.querySelector('.error').innerHTML = '';
-        emailAddressInput.classList.remove('error');
+        /* sprawdzenie adresu email i ewentualne pokolorwanie inputa */
+        var emailAddressInput = event.target.querySelector('input[id="input-email"]');
+
+        if(emailAddressInput.value.indexOf('@') < 0) {
+            isFormValidate = false;
+            emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+            emailAddressInput.classList.add('error');
+        } else {
+            emailAddressInput.parentElement.querySelector('.error').innerHTML = '';
+            emailAddressInput.classList.remove('error');
+        }
+
+        event.cancel = true;
+        return !isFormValid ? event.preventDefault() : true;
+    });
+}
+
+
+// kontrola modali
+
+var modal = document.getElementById('overlay');
+
+// if any modal exists
+if(typeof modal !== "undefined" && modal !== null) {
+    // define function to close modals
+    function closeModal() {
+        document.getElementById('overlay').classList.remove('show');
     }
 
-    event.cancel = true;
-    return !isFormValid ? event.preventDefault() : true;
-});
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            closeModal();
+        })
+    });
+
+    document.querySelector('#overlay').addEventListener('click', function(e) {
+        if(e.target === this) {
+            closeModal()
+        }
+    });
+
+    document.addEventListener('keyup', function(e) {
+        if(e.keyCode === 27) {
+            closeModal()
+        }
+    });
+
+    function openModal(modal) {
+        document.querySelectorAll('#overlay > *').forEach(function(modal) {
+            modal.classList.remove('show')
+        });
+        document.querySelector('#overlay').classList.add('show');
+        document.querySelector(modal).classList.add('show');
+    }
+
+    var buttonAddLink = document.getElementsByClassName('btn-modal--add-link');
+    if(typeof buttonAddLink !== "undefined" && buttonAddLink !== null) {
+
+        Array.from(buttonAddLink).forEach(function(btn) {
+            btn.addEventListener('click', function (e) {
+                e.preventDefault();
+                openModal('#addLinkModal');
+            });
+        });
+    }
+
+};
+
