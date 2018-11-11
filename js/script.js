02a8fc086a925e20a6c0ba4ed8713baa6222c909
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2018-11-11 19:25:56 +0100

    Add email address validation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 /* toggle menu */
 
 function toggleMenu(visible) {
-    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-md', visible)
-    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-sm', visible)
-    document.querySelector('.layout-row-top-right').classList.toggle('noshow-sm', visible)
+    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-md', visible);
+    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-sm', visible);
+    document.querySelector('.layout-row-top-right').classList.toggle('noshow-sm', visible);
 }
 
 document.querySelector('.navigation__menu-button').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleMenu()
+    toggleMenu();
 });
 
 /* toggle menu end */
 
+document.querySelector('form#personal-data').addEventListener('submit', function(event) {
+    var isFormValid = true;
 
-/* toggle menu mobile */
-// aim: on mobile when opening menu - hide all other elements
+    /* sprawdzenie adresu email i ewentualne pokolorwanie inputa */
+    var emailAddressInput = event.target.querySelector('input[id="input-email"]');
 
-/* toggle menu mobile end*/
+    if(emailAddressInput.value.indexOf('@') < 0) {
+        isFormValidate = false;
+        emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+        emailAddressInput.classList.add('error');
+    } else {
+        emailAddressInput.parentElement.querySelector('.error').innerHTML = '';
+        emailAddressInput.classList.remove('error');
+    }
+
+    event.cancel = true;
+    return !isFormValid ? event.preventDefault() : true;
+});
