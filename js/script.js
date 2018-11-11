5ef60ef94b793c913646d9ac7bcd6e026ef4df7f
Łukasz Kulbacki <lukasz.kulbacki@datality.co>
2018-11-11 14:43:15 +0100

    Fix menu toggle for various resolutions by defining and toggling proper show/noshow class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 /* toggle menu */
 
 function toggleMenu(visible) {
-    document.querySelector('.layout-row-bottom-left').classList.toggle('show', visible)
-    document.querySelector('.layout-row-top-right').classList.toggle('noshow', visible)
+    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-md', visible)
+    document.querySelector('.layout-row-bottom-left').classList.toggle('noshow-sm', visible)
+    document.querySelector('.layout-row-top-right').classList.toggle('noshow-sm', visible)
 }
 
 document.querySelector('.navigation__menu-button').addEventListener('click', function(e) {
